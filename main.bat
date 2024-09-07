c56e0da9d54d73ceb43a5b81363e0e916d6074b2
Addison McIlraith <gamingisscience@gmail.com>
2024-09-07 11:26:53 -0500

    Updates main.bat

--- main.bat
+++ main.bat
@@ -15,9 +15,11 @@ echo.
 echo Select what you'd like to do to play TF2.
 echo.
 echo 1. Start Normally
-echo 2. Update Configs
-echo 3. Set Casual Mods
-echo 4. Set Competitive Mods
+echo 2. Update Configs Only
+echo 3. Set Casual Configuration Only
+echo 4. Set Competitive Configuration Only
+echo 5. Start in Casual Configuration
+echo 6. Start in Competitive Configuration
 echo.
 
 :getOptions
@@ -38,40 +40,18 @@ exit
 
 :option-1
 cd "%tf2-path%\tf\custom"
-cls
-IF NOT EXIST "..\..\tf_win64.exe" (goto :WAIT)
-goto :DOIT
-
-:WAIT
-color c
-echo Error Code : E01 (not in custom folder)
-goto :ASK
-
-:ASK
-set /P c=This program might not be in the CUSTOM folder, continue? [continue or cancel]
-if /I %c% EQU continue goto :DOIT
-if /I %c% EQU cancel goto :EXIT2
-
-:DOIT
-echo Removing cache files.
 del /F /S *.cache
-goto EXIT
-
-:EXIT2
-exit
-
-:EXIT
 "Team Fortress 2.url"
 exit
 
 :option-2
 xcopy /e /k /h /i /y "cfg\*" "%tf2-path%\tf\cfg\*"
+xcopy /e /k /h /i /y "cfg\*" "%tf2-path%\tf\cfg\overrides\*"
 start "" "%~f0"
 exit
 
 :option-3
 del "%tf2-path%\tf\custom\*" /s/q
-xcopy /e /k /h /i /y "custom_casual\*" "%tf2-path%\tf\custom\overrides\"
 xcopy /e /k /h /i /y "custom_casual\*" "%tf2-path%\tf\custom\"
 xcopy /e /k /h /i /y "autoexec_casual\autoexec.cfg" "cfg\"
 start "" "%~f0"
@@ -82,4 +62,23 @@ del "%tf2-path%\tf\custom\*" /s/q
 xcopy /e /k /h /i /y "custom_comp\*" "%tf2-path%\tf\custom\"
 xcopy /e /k /h /i /y "autoexec_comp\autoexec.cfg" "cfg\"
 start "" "%~f0"
+exit
+
+:option-5
+del "%tf2-path%\tf\custom\*" /s/q
+xcopy /e /k /h /i /y "custom_casual\*" "%tf2-path%\tf\custom\overrides\"
+xcopy /e /k /h /i /y "custom_casual\*" "%tf2-path%\tf\custom\"
+xcopy /e /k /h /i /y "autoexec_casual\autoexec.cfg" "cfg\"
+
+:option-6
+del "%tf2-path%\tf\custom\*" /s/q
+xcopy /e /k /h /i /y "custom_comp\*" "%tf2-path%\tf\custom\"
+xcopy /e /k /h /i /y "autoexec_comp\autoexec.cfg" "cfg\"
+
+xcopy /e /k /h /i /y "cfg\*" "%tf2-path%\tf\cfg\*"
+xcopy /e /k /h /i /y "cfg\*" "%tf2-path%\tf\cfg\overrides\*"
+
+cd "%tf2-path%\tf\custom"
+del /F /S *.cache
+"Team Fortress 2.url"
 exit
